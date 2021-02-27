c4496d02d0558abf381726ab578dcf92bf30130a
dingqunfei <dqflying@gmail.com>
2021-02-28 00:26:47 +0800

    add

--- Learn/binary-tree/3_2.cpp
+++ Learn/binary-tree/3_2.cpp
@@ -21,3 +21,47 @@
  * 
  */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) 
+    {
+        vector<int> res;
+        stack<pair<TreeNode *, bool>> nodes;
+        while(root || !nodes.empty())
+        {
+            while(root)
+            {
+                nodes.push(make_pair(root, true));
+                root = root->left;
+            }
+            pair<TreeNode *, bool> node = nodes.top();
+            if(node.second)
+            {
+                nodes.pop();
+                node.second = false;
+                nodes.push(node);
+                root = node.first;
+                root = root->right;
+            }
+            else
+            {
+                root = node.first;
+                res.push_back(root->val);
+                nodes.pop();
+                root = nullptr;
+            }
+        }
+        return res;
+    }
+};
