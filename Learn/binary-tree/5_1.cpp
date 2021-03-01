27c99f11394ee14c9f89dc5d529365cc50d4777d
dingqunfei <dqflying@gmail.com>
2021-03-01 23:19:59 +0800

    add

---
+++ Learn/binary-tree/5_1.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file 5_1.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-03-01
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-03-01
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) 
+    {
+        if(!root)
+        {
+            return 0;
+        }
+        else
+        {
+            return max(maxDepth(root->left), maxDepth(root->right))+1;
+        }
+    }
+};
