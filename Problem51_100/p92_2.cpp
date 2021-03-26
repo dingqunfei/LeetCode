f3a96110a7cbf5cd68fbc507a004baf99e15f52e
dingqunfei <dqflying@gmail.com>
2021-03-27 01:30:52 +0800

    add

---
+++ Problem51_100/p92_2.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file p92_2.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-03-27
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-03-27
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
