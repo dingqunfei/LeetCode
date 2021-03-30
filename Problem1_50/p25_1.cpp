1a38b4af0206c397d823f1128e4f2e916fed72bf
dingqunfei <dqflying@gmail.com>
2021-03-31 01:03:32 +0800

    add

--- Problem1_50/p25_1.cpp
+++ Problem1_50/p25_1.cpp
@@ -21,3 +21,45 @@
  * 
  */
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode *tmphead = head;
+        int count = k;
+        while(tmphead && count > 0)
+        {
+            tmphead = tmphead->next;
+            --count;
+        }
+        if(count > 0)
+        {
+            return head;
+        }
+        ListNode *prev = NULL;
+        ListNode *curr = head;
+        ListNode *next = NULL;
+        count = k;
+        while(curr && count > 0)
+        {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            --count;
+        }
+        ListNode *last = reverseKGroup(curr, k);
+        head->next = last;
+
+        return prev;
+    }
+};
