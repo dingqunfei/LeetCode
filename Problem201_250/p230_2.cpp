b86beed676dc3d88882e9a065d4612be6b1ec21c
dingqunfei <dqflying@gmail.com>
2021-04-05 15:53:11 +0800

    add

---
+++ Problem201_250/p230_2.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file p230_2.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-04-05
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-04-05
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
