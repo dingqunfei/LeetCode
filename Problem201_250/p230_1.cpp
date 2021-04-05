a318e854c392b935d9d5e8f0ce03e47860176b72
dingqunfei <dqflying@gmail.com>
2021-04-05 15:51:56 +0800

    add

---
+++ Problem201_250/p230_1.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file p230_1.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-04-05
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-04-05
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void traverse(TreeNode *root, vector<int> &res)
+    {
+        if(!root)
+        {
+            return;
+        }
+        traverse(root->left, res);
+        res.push_back(root->val);
+        traverse(root->right, res);
+
+    }
+
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> res;
+        traverse(root, res);
+        return res[k-1];
+    }
+};
