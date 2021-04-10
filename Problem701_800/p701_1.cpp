669801351f4c5ce09c233c41ba27e5b42afb6e72
dingqunfei <dqflying@gmail.com>
2021-04-10 12:19:28 +0800

    add

---
+++ Problem701_800/p701_1.cpp
@@ -0,0 +1,52 @@
+/**
+ * @file p701_1.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-04-10
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-04-10
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(!root)
+        {
+            return new TreeNode(val);
+        }
+        if(val < root->val)
+        {
+            root->left = insertIntoBST(root->left, val);
+        }
+        else
+        {
+            root->right = insertIntoBST(root->right, val);
+        }
+        return root;
+    }
+};
