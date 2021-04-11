b24f3bf87947c2301bb36c6708d2868c541958c7
dingqunfei <dqflying@gmail.com>
2021-04-11 18:13:50 +0800

    add

---
+++ Problem201_250/p236_1.cpp
@@ -0,0 +1,57 @@
+/**
+ * @file p236_1.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-04-11
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-04-11
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution { 
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)
+        {
+            return nullptr;
+        }
+        if(root == p || root == q)
+        {
+            return root;
+        }
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        if(left && right)
+        {
+            return root;
+        }
+        if(!left && !right)
+        {
+            return nullptr;
+        }
+        return left?left:right;
+    }
+};
