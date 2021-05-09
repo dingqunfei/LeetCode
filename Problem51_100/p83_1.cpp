abf94421784b5e7580103d61e1db6fad7851406d
dingqunfei <dqflying@gmail.com>
2021-05-09 18:24:34 +0800

    add

---
+++ Problem51_100/p83_1.cpp
@@ -0,0 +1,53 @@
+/**
+ * @file p83_1.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-05-09
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-05-09
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while(fast && fast->next)
+        {
+            fast = fast->next;
+            if(slow->val != fast->val)
+            {
+                slow->next = fast;
+                slow = slow->next;
+            }
+        }
+        if(slow)
+        {
+            slow->next = nullptr;
+        }
+        return head;
+    }
+};
