27018012d1e7067dada1b9bd2be536504ff455a9
dingqunfei <dqflying@gmail.com>
2021-05-11 22:19:17 +0800

    add

--- Problem1_50/p46_1.cpp
+++ Problem1_50/p46_1.cpp
@@ -56,3 +56,49 @@ public:
         return res;
     }
 };
+
+
+
+//修改，使用数组used 为nums中元素是否使用做标记
+/*
+class Solution {
+private:
+    vector<vector<int>> res;
+
+public:
+    void backtrack(vector<int> &nums, vector<int> &track, bool *used)
+    {
+        if(track.size() == nums.size())
+        {
+            res.push_back(track);
+            return;
+        }
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(used[i])
+            {
+                continue;
+            }
+            track.push_back(nums[i]);
+            used[i] = true;
+            backtrack(nums, track, used);
+            track.pop_back();
+            used[i] = false;
+        }
+
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        vector<int> track;
+        int len = nums.size();
+        bool used[len];
+        for(int i = 0; i < len; ++i)
+        {
+            used[i] = false;
+        }
+        backtrack(nums, track, used);
+        return res;
+    }
+};
+*/
