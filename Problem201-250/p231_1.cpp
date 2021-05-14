004039a6028d00b93bf1c983335fbbca7fb1105a
dingqunfei <dqflying@gmail.com>
2021-05-15 02:06:18 +0800

    add

---
+++ Problem201-250/p231_1.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file p231_1.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-05-15
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-05-15
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0)
+        {
+            return false;
+        }
+        return (n&(n-1)) == 0;
+    }
+};
