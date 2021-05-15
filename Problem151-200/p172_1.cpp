d4b5d691ac97ac3b06e3626c430df73658853e50
dingqunfei <dqflying@gmail.com>
2021-05-15 16:47:39 +0800

    add

---
+++ Problem151-200/p172_1.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file p172_1.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-05-15
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-05-15
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int count = 0;
+        while(n >= 5)
+        {
+            count += n/5;
+            n = n/5;
+        }
+        return count;
+    }
+};
