d44cfd244d098e0939da2aa611a9c6ea0a25f92e
dingqunfei <dqflying@gmail.com>
2021-05-23 10:51:25 +0800

    p22 solution 1

---
+++ Problem1-50/p22_1.cpp
@@ -0,0 +1,50 @@
+/**
+ * @file p22_1.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-05-23
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-05-23
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+//回溯法，在字符串中，不断插入一个"()"，直到长度达到2*n，使用set去除重复元素
+//耗时较长，内存占用较多
+
+class Solution {
+private:
+    set<string> res;
+    void backtrack(string track, int n)
+    {
+        if(track.size() == 2*n)
+        {
+            res.insert(track);
+            return;
+        }
+        for(int i = 0; i <= track.size(); ++i)
+        {
+            track.insert(i, "()");
+            backtrack(track, n);
+            track.erase(i, 2);
+        }
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        string track;
+        backtrack(track, n);
+        return vector(res.begin(), res.end());
+    }
+};
