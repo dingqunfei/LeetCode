4000741ab23d96646eab1ec2b0dc7cc112dad8ba
dingqunfei <dqflying@gmail.com>
2021-05-28 23:08:26 +0800

    add

--- Problem701-800/p724_1.cpp
+++ Problem701-800/p724_1.cpp
@@ -44,3 +44,20 @@ public:
         return -1;
     }
 };
+
+
+//优化版本，直接计算最终的sum和
+class Solution {
+public:
+    int pivotIndex(vector<int> &nums) {
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        int sum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (2 * sum + nums[i] == total) {
+                return i;
+            }
+            sum += nums[i];
+        }
+        return -1;
+    }
+};
