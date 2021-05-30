3a84062d4483060376a03927e68da09fa27b8fe4
dingqunfei <dqflying@gmail.com>
2021-05-31 02:01:37 +0800

    add

---
+++ Problem351-400/p382_1.cpp
@@ -0,0 +1,67 @@
+/**
+ * @file p382_1.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-05-31
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-05-31
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode *h;
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        h = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        int i = 0, res = 0;
+        ListNode *p = h;
+        while(p)
+        {
+            ++i;
+            if(rand()%i == 0)
+            {
+                res = p->val;
+            }
+            p = p->next;
+        }
+        return res;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
