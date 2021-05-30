8768b42adaa16a0c9111ddd3f2ce136dd6fe3be7
dingqunfei <dqflying@gmail.com>
2021-05-30 12:01:49 +0800

    add

---
+++ Problem1001-1500/p1109_2.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file p1109_2.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-05-30
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-05-30
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+//优化了最后一个元素
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> seats(n+1, 0);
+
+        //seats[0] = bookings[0][2];
+
+        for(int i = 0; i < bookings.size(); ++i)
+        {
+            seats[bookings[i][0]-1] += bookings[i][2];
+            
+            seats[bookings[i][1]] -= bookings[i][2];
+        }
+        for(int i = 1; i < n; ++i)
+        {
+            seats[i] += seats[i-1];
+        }
+        seats.pop_back();
+        return seats;
+    }
+};
