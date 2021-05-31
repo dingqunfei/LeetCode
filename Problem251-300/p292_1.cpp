308d0cc485317fb751c1e0ba641df93a6cb1eaeb
dingqunfei <dqflying@gmail.com>
2021-05-31 23:41:25 +0800

    add

---
+++ Problem251-300/p292_1.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file p292_1.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-05-31
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-05-31
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return !(n%4 == 0);
+    }
+};
