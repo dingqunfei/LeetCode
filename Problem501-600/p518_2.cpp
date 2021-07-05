e071596289000037f58313cf98faefd3dab15e45
dingqunfei <dqflying@gmail.com>
2021-07-06 01:15:08 +0800

    add

---
+++ Problem501-600/p518_2.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file p518_2.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-07-06
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-07-06
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        
+        vector<int> dp(amount + 1);
+        dp[0] = 1;
+        for (int& coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+
+    }
+};
