bc3072df80b5abbc79670b70ec93082c62f6950b
dingqunfei <dqflying@gmail.com>
2021-08-05 00:47:54 +0800

    add

---
+++ Problem151-200/p174_2.cpp
@@ -0,0 +1,38 @@
+/**
+ * @file p174_2.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-08-05
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-08-05
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int n = dungeon.size(), m = dungeon[0].size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MAX));
+        dp[n][m - 1] = dp[n - 1][m] = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = m - 1; j >= 0; --j) {
+                int minn = min(dp[i + 1][j], dp[i][j + 1]);
+                dp[i][j] = max(minn - dungeon[i][j], 1);
+            }
+        }
+        return dp[0][0];
+    }
+};
