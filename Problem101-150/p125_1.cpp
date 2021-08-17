c53ee886072b714d752c30e739857998203dd8c8
dingqunfei <dqflying@gmail.com>
2021-08-18 01:09:04 +0800

    add

---
+++ Problem101-150/p125_1.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file p125_1.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-08-18
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-08-18
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string sgood;
+        for (char ch: s) {
+            if (isalnum(ch)) {
+                sgood += tolower(ch);
+            }
+        }
+        int n = sgood.size();
+        int left = 0, right = n - 1;
+        while (left < right) {
+           if (sgood[left] != sgood[right]) {
+                return false;
+            }
+            ++left;
+            --right;
+        }
+        return true;
+    }
+};
