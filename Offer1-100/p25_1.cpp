02aceed65898cc3c9fc72c69a9c7837a6d3b3a04
dingqunfei <dqflying@gmail.com>
2021-08-27 01:17:44 +0800

    add

---
+++ Offer1-100/p25_1.cpp
@@ -0,0 +1,65 @@
+/**
+ * @file p25_1.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2021-08-27
+ * 
+ * @copyright Copyright (c) 2021 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2021-08-27
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        
+        ListNode *dummy = new ListNode(0);
+        ListNode *head = dummy;
+        while(l1 && l2)
+        {
+            if(l1->val > l2->val)
+            {
+                head->next = l2;
+                l2 = l2->next;
+                head = head->next;
+            }
+            else
+            {
+                head->next = l1;
+                l1 = l1->next;
+                head = head->next;
+            }
+        }
+        if(l1)
+        {
+            head->next = l1;
+        }
+        else if(l2)
+        {
+            head->next = l2;
+        }
+        return dummy->next;
+
+    }
+};
