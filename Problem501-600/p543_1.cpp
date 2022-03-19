255dc0838e52b275bf02cc08ef5d1543242fa58c
dingqunfei <dqflying@gmail.com>
2022-03-19 10:34:48 +0800

    add

---
+++ Problem501-600/p543_1.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root)
+    {
+        if(root == NULL)
+        {
+            return 0;
+        }
+        return 1+max(maxDepth(root->left), maxDepth(root->right));
+    }
+    int res = INT_MIN;
+    void traverse(TreeNode* root) {
+        if(root == NULL)
+        {
+            return;
+        }
+        int depth = maxDepth(root->left) + maxDepth(root->right);
+        res = max(res, depth);
+        traverse(root->left);
+        traverse(root->right);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        traverse(root);
+        return res;
+    }
+};
