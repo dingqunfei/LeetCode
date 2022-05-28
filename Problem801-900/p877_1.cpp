70e66d98ecfcdaae1c5a1983eb04c4ab39540f03
dingqunfei <dqflying@gmail.com>
2022-05-28 20:24:07 +0800

    add

---
+++ Problem801-900/p877_1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int i = 0; i < n; ++i)
+        {
+            dp[i][i] = piles[i];
+        }
+        for(int i = n-2; i >= 0; --i)
+        {
+            for(int j = i+1; j < n; ++j)
+            {
+                dp[i][j] = max(piles[i] - dp[i+1][j], piles[j] - dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1] > 0;
+    }
+};
