45b7721a5c92fa5a1a690e19ddfadc997963147f
dingqunfei <dqflying@gmail.com>
2022-07-22 21:44:19 +0800

    add

--- Problem801-900/p870_1.cpp
+++ Problem801-900/p870_1.cpp
@@ -1,3 +1,40 @@
+
+class Solution {
+public:
+    vector<int> advantageCount(vector<int>& nums1, vector<int>& nums2) {
+        vector<pair<int, int>> index;
+        int n = nums2.size();
+        for(int i = 0; i < n; ++i)
+        {
+            index.push_back({nums2[i], i});
+        }
+        sort(nums1.begin(), nums1.end());
+        sort(index.begin(), index.end(), [](auto &a, auto &b){return a.first < b.first;});
+
+        int a = n-1, b = n-1;
+        int aStart = 0;
+        vector<int> vec(n, INT_MAX);
+        while(a >= 0 && b >= 0 && aStart < n)
+        {
+              if(nums1[a] > index[b].first)
+              {
+                  vec[index[b].second] = nums1[a];
+                  --a;
+                  --b;
+              }
+              else
+              {
+                  vec[index[b].second] = nums1[aStart];
+                  ++aStart;
+                  --b;
+              }
+        }
+        
+        return vec;
+    }
+};
+
+
 class Solution {
 public:
     vector<int> advantageCount(vector<int>& nums1, vector<int>& nums2) {
